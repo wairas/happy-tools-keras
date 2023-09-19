e45af4c05b4d85c54e05ae5ee8e1ca6d29492480
Peter Reutemann <fracpete@gmail.com>
2023-09-20 11:41:52 +1200

    moved console tools into "console" sub-package and gui ones into "gui" sub-package

--- setup.py
+++ setup.py
@@ -40,9 +40,9 @@ setup(
     packages=find_namespace_packages(where='src'),
     entry_points={
         "console_scripts": [
-            "happy-keras-pixel-regression-build=happy_keras.builders.pixel_regression_build:sys_main",
-            "happy-keras-segmentation-build=happy_keras.builders.segmentation_build:sys_main",
-            "happy-keras-unsupervised-build=happy_keras.builders.segmentation_build:sys_main",
+            "happy-keras-pixel-regression-build=happy_keras.console.builders.pixel_regression_build:sys_main",
+            "happy-keras-segmentation-build=happy_keras.console.builders.segmentation_build:sys_main",
+            "happy-keras-unsupervised-build=happy_keras.console.builders.segmentation_build:sys_main",
         ]
     },
     version="0.0.1",
